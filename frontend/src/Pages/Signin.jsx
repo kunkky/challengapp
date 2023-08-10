551cb573a26e8f399070f9297a907e9524ecfb4c
kunkky <83945048+kunkky@users.noreply.github.com>
2023-08-10 12:20:48 +0100

    Update Signin.jsx

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -9,7 +9,6 @@ const Signin = () => {
           <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
               <Link>
                   <img class="w-[160px] mb-6 mr-2" src={Logo} alt="logo" />
-
               </Link>
               <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                   <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
