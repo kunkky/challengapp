2dbb7c605b4e00c349de56a41892fc8d661a25dd
kunkky <83945048+kunkky@users.noreply.github.com>
2023-08-10 15:06:00 +0100

    dashboard Template added

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -5,6 +5,7 @@ import Home from './Home'
 
 
 const Signin = () => {
+
   return (
       <section class="bg-gray-50 dark:bg-gray-900">
           <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
