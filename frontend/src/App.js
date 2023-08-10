4b7ff8babda2e9d9ad0bc61a65282187abb1cc51
kunkky <83945048+kunkky@users.noreply.github.com>
2023-08-10 10:50:19 +0100

    routes set for the front end
    I added route to the front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from './Pages/Home'
+import SignUp from 'Pages/SignUp'
+import Signin from 'Pages/Signin'
+import Profile from 'Pages/Profile'
+import ErrorPage from 'Pages/ErrorPage'
+import Dashboard from 'Pages/Dashboard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+const ProtectedRoutes = ({ children }) => {
+  if (sessionStorage.getItem("token")) {
+    return children
+  } else {
+    return <Navigate to={"/"} replace={true} />
+  }
 }
 
-export default App;
+
+
+const App = () => {
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/signUp' element={<SignUp />}></Route>
+      <Route path='/signin' element={<Signin />}></Route>
+      {/* Protected routes */}
+
+      <Route path='/dashboard' element={
+        <ProtectedRoutes>
+
+          <Dashboard />
+        </ProtectedRoutes>
+      }></Route>
+
+      <Route path='/profile' element={
+        <ProtectedRoutes>
+          <Profile />
+        </ProtectedRoutes>
+      }></Route>
+
+      {/* Error Page */}
+      <Route path='*' element={<ErrorPage />}></Route>
+    </Routes>
+  )
+}
+
+export default App
