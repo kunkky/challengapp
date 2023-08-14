8ad5061fed7ae25cd549bc705173a6188cd8eda9
kunkky <83945048+kunkky@users.noreply.github.com>
2023-08-13 20:54:52 -1200

    login done

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/images/logo.png' 
+import { useFormik } from 'formik'; //for processing forms 
+import * as Yup from 'yup'; //for form validation
+
 
 
 const Signin = () => {
+
+
+    const SignupForm = () => {
+        const formik = useFormik({
+            initialValues: {
+                password: '',
+                email: '',
+            },
+            validationSchema: Yup.object({
+                password: Yup.string().required('Required'),
+                email: Yup.string().email('Invalid email address').required('Required'),
+            }),
+            onSubmit: values => {
+                alert(JSON.stringify(values, null, 2));
+            },
+        });
+
 
   return (
       <section class="bg-gray-50 dark:bg-gray-900">
@@ -16,14 +36,36 @@ const Signin = () => {
                       <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                           Sign In 
                       </h1>
-                      <form class="space-y-4 md:space-y-6" action="#">
+                      <form class="space-y-4 md:space-y-6" action="#" onSubmit={formik.handleSubmit}>
                           <div>
                               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                              <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
-                          </div>
+                              <input
+                                  id="email"
+                                  name="email"
+                                  type="email"
+                                  onChange={formik.handleChange}
+                                  onBlur={formik.handleBlur}
+                                  value={formik.values.email}
+                                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                              {formik.touched.email && formik.errors.email ? (
+                                  <div>{formik.errors.email}</div>
+                              ) : null}
+                            </div>
                           <div>
                               <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                              <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                              <input 
+                              
+                                  id="password"
+                                  name="password"
+                                  type="password"
+                                  onChange={formik.handleChange}
+                                  onBlur={formik.handleBlur}
+                                  value={formik.values.password}
+                              
+                               class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                              {formik.touched.password && formik.errors.password ? (
+                                  <div>{formik.errors.password}</div>
+                              ) : null}
                           </div>
                           
                           
