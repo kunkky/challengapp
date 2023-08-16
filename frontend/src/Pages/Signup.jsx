d883c6a441a1bfee97df45952f082b9be9112d58
Deiyveed <oladipupoolumide863@gmail.com>
2023-08-16 14:25:13 +0100

    signup design work

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -93,9 +93,9 @@ const Signup = () => {
     };
 
     return (
-        <section class="bg-gray-50 dark:bg-gray-900">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <Link to='/' class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <section class="bg-gray-50 dark:bg-gray-900 lg:h-[140svh] md:h-[105svh] pt-7">
+            <div class="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <Link to='/' class="flex items-center mb-4 text-2xl font-semibold text-gray-900 dark:text-white">
                     <img className="w-[150px] mb-5" src={Logo} alt="logo" />
                 </Link>
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
