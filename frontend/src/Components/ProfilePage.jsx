af50eb564f1f65b3b0e9a620d89bf88c8cef2207
Haroun Ayotunde <129394388+hocbabane@users.noreply.github.com>
2023-08-19 20:47:45 +0100

    Update ProfilePage.jsx

--- frontend/src/Components/ProfilePage.jsx
+++ frontend/src/Components/ProfilePage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 
 const ProfilePage = () => {
-  const [profileDetails, setProfileDetails] = useState({});
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     fetchProfileDetails();
@@ -30,12 +30,12 @@ const ProfilePage = () => {
     <div>
       <div className="p-4">
         <div className="bg-white-200 mb-4">
-          <h2 className="text-blue-2xl font-semibold mb-2"><strong>Student Name:</strong> {profileDetails.name}</h2>
-          <p className="text-blue-600 mb-1"><strong>Stack:</strong> {profileDetails.stack}</p>
-          <p className="text-blue-600 mb-1"><strong>Level:</strong> {profileDetails.level}</p>
-          <p className="text-blue-600 mb-1"><strong>Language:</strong> {profileDetails.language}</p>
-          <p className="text-blue-600 mb-1"><strong>Email:</strong> {profileDetails.email}</p>
-          <p className="text-blue-600 mb-1"><strong>Contact:</strong> {profileDetails.contact}</p>
+          <h2 className="text-blue-2xl font-semibold mb-2"><strong>Student Name:</strong> {user.name}</h2>
+          <p className="text-blue-600 mb-1"><strong>Stack:</strong> {user.stack}</p>
+          <p className="text-blue-600 mb-1"><strong>Level:</strong> {user.level}</p>
+          <p className="text-blue-600 mb-1"><strong>Language:</strong> {user.language}</p>
+          <p className="text-blue-600 mb-1"><strong>Email:</strong> {user.email}</p>
+          <p className="text-blue-600 mb-1"><strong>Contact:</strong> {user.contact}</p>
         </div>
         <button
           className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded"
