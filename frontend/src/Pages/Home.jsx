1b4e76e9dadd9fdccd14a6f84b893be1499f2db3
Davidmide Adenusi <Davidmide07@gmail.com>
2023-08-19 15:40:22 +0100

    adjust welcome text

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
           <div className="container relative mx-auto">
             <div className="items-center flex flex-wrap">
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-                <div className="pr-12">
+                <div className="pr-12 mt-8">
                   <h1 className="text-white font-semibold text-5xl">
                     Welcome to the ultimate Tech Challenge website, where
                     innovation meets competition!
