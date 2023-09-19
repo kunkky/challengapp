7e731d5abee6986e4b78ac60d5b19aecec3571eb
kunkky <83945048+kunkky@users.noreply.github.com>
2023-09-18 23:19:39 -1200

    Update BaseUrl.js

--- frontend/src/BaseUrl.js
+++ frontend/src/BaseUrl.js
@@ -1,5 +1,5 @@
 
-const BaseUrl = 'https://techchallengeapp.cyclic.app/api/v1/';
+const BaseUrl = 'https://techchallenge.onrender.com/api/v1/';
 
 
 // const BaseUrl = 'http://localhost:2994/api/v1/'; //uncomment this and comment the first if workinglocallay
