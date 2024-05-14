530fc7cf061992b1674c49ac7c84f098ee6039c0
kunkky <kunkkykukky@gmail.com>
2024-05-14 15:28:17 +0200

    Update BaseUrl.js

--- frontend/src/BaseUrl.js
+++ frontend/src/BaseUrl.js
@@ -1,8 +1,7 @@
 
-const BaseUrl = 'https://techchallenge.onrender.com/api/v1/';
+const BaseUrl = 'https://challengappbackend.onrender.com/api/v1/';
 
 
-// const BaseUrl = 'http://localhost:2994/api/v1/'; //uncomment this and comment the first if workinglocallay
 
 
 export default BaseUrl
